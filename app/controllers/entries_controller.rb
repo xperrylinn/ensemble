0fdfc2d1783f55b0854160ded9d60b1a59892739
Xavier <xperrylinn@berkeley.edu>
2020-05-25 10:39:46 -0700

    basic crud for entries complete.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,15 +6,40 @@ class EntriesController < ApplicationController
     def show
         @entry = Entry.find(params[:id])
     end
-    
+
     def new
+        @entry = Entry.new
+    end
+
+    def edit
+        @entry = Entry.find(params[:id])
     end
 
     def create
         @entry = Entry.new(entry_params)
        
-        @entry.save
-        redirect_to @entry
+        if @entry.save
+          redirect_to @entry
+        else
+          render 'new'
+        end
+    end
+
+    def update
+        @entry = Entry.find(params[:id])
+       
+        if @entry.update(entry_params)
+          redirect_to @entry
+        else
+          render 'edit'
+        end
+    end
+
+    def destroy
+        @entry = Entry.find(params[:id])
+        @entry.destroy
+       
+        redirect_to entries_path
     end
        
     private
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,4 @@
 class Entry < ApplicationRecord
+    validates :title, presence: true, 
+                      length: { minimum: 5 }
 end
